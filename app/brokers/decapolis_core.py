2c8634f8a99148a7a538e62a6ea4400ed6a42bd5
Hisham <h.nasralla@decapolis.io>
2023-03-07 14:41:24 +0300

    print submit response from submit api

--- app/brokers/decapolis_core.py
+++ app/brokers/decapolis_core.py
@@ -29,6 +29,9 @@ class CoreApplicationBroker(Broker):
 
 def send_collected_data(company_id, process_id, data):
     host = os.environ.get('PRIVATE_CORE_ENDPOINT')
+    headers = {
+        "Host", "parser:8000"
+    }
     url = f"{host}process/{process_id}/company/{company_id}/active_process/submit"
     response = requests.request("POST", url, data=data)
     return response
