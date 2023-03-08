636402fe0d441db7ea7d7c01ef6f89995b52678b
Hisham <h.nasralla@decapolis.io>
2023-03-08 12:52:51 +0300

    update env variable for private back end and update the code based on that

--- app/brokers/decapolis_core.py
+++ app/brokers/decapolis_core.py
@@ -29,9 +29,9 @@ class CoreApplicationBroker(Broker):
 
 def send_collected_data(company_id, process_id, data):
     host = os.environ.get('PRIVATE_CORE_ENDPOINT')
-    # headers = {
-    #     "Host": "parser-dev.decapolis.io"
-    # }
-    url = f"http://dev-app-private.decapolis.io/api/v2/process/{process_id}/company/{company_id}/active_process/submit"
-    response = requests.request("POST", url, data=data)
+    headers = {
+        "Host": "parser-dev.decapolis.io"
+    }
+    url = f"http://127.0.0.1:8001/api/v2/process/{process_id}/comapny/{company_id}/active_process/submit"
+    response = requests.request("POST", url, headers=headers, data=data)
     return response
