17210ce84d0f46ecee4a958d0e4ee5ae6809851e
Hisham <h.nasralla@decapolis.io>
2023-04-13 02:00:20 +0300

    .

--- app/api/v1/routers/__init__.py
+++ app/api/v1/routers/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from app.api.v1.views import parser_router, beat_router, profile_router
+from app.api.v1.views import parser_router, beat_router, profile_router, queue_router
 
 api_router = APIRouter()
 api_router.include_router(parser_router, tags=["parser"], prefix="/parser")
-api_router.include_router(profile_router, tags=["profile"], prefix="/parser")
+api_router.include_router(profile_router, tags=["profile"], prefix="/profile")
+api_router.include_router(queue_router, tags=["queue"], prefix="/queue")
 api_router.include_router(beat_router, tags=["beat"])
--- app/api/v1/views/__init__.py
+++ app/api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 from app.api.v1.views.health_check import router as beat_router
 from app.api.v1.views.profile import router as profile_router
 from app.api.v1.views.csv import router as parser_router
+from app.api.v1.views.queue import router as queue_router
 
-__all__ = ("parser_router", "beat_router", "profile_router")
+__all__ = ("parser_router", "beat_router", "profile_router", "queue_router")
