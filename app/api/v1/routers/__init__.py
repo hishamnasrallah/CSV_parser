d5ca3304373fa9158dd3adebff19402ca455d0ee
Hisham <h.nasralla@decapolis.io>
2023-04-13 12:49:41 +0300

    fix url

--- app/api/v1/routers/__init__.py
+++ app/api/v1/routers/__init__.py
@@ -3,6 +3,6 @@ from app.api.v1.views import parser_router, beat_router, profile_router, queue_r
 
 api_router = APIRouter()
 api_router.include_router(parser_router, tags=["parser"], prefix="/parser")
-api_router.include_router(profile_router, tags=["profile"], prefix="/profile")
+api_router.include_router(profile_router, tags=["profile"], prefix="/parser")
 api_router.include_router(queue_router, tags=["queue"], prefix="/queue")
 api_router.include_router(beat_router, tags=["beat"])
